9ba24087d1e950e9757ff0d08f1a7a00e19a9f6c
Chen chiheng <chenchiheng123@163.com>
2016-05-09 23:09:32 +0800

    create the basic frame and item

---
+++ logstat/item.py
@@ -0,0 +1,20 @@
+#encoding:utf-8
+
+class BasicItem(object):
+    def __init__(self):
+        super(BasicItem, self).__init__()
+
+class CPU(BasicItem):
+    def __init__(self):
+        super(CPU, self).__init__()
+
+class MEM(BasicItem):
+    def __init__(self):
+        super(MEM, self).__init__()
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
